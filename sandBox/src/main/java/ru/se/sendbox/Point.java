424d683e2e33e43bd1241d042c1bab7cdfc4d8e7
Ekaterina Serkova <48721923+KateSerkova@users.noreply.github.com>
2022-05-06 18:01:49 +0300

    Исправленное Задание №2

--- sandBox/src/main/java/ru/se/sendbox/Point.java
+++ sandBox/src/main/java/ru/se/sendbox/Point.java
@@ -2,21 +2,15 @@ package ru.se.sendbox;
 
 public class Point {
 
-  public double x1; // атрибут
-  public double y1;
-  public double x2;
-  public double y2;
+  public double x; // атрибут
+  public double y;
 
-  public Point (double x1, double y1, double x2, double y2) {  // это конструктор
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
+  public Point(double x, double y) {  // это конструктор
+    this.x = x;
+    this.y = y;
   }
 
-  public double distance (){
-
-    return Math.sqrt((this.x1 - this.x2)* 2 + (this.y1 - this.y2)* 2);
-
+  public double distance (Point p2){
+    return Math.sqrt((this.x - p2.x)* 2 + (this.y - p2.y)* 2);
   }
 }
