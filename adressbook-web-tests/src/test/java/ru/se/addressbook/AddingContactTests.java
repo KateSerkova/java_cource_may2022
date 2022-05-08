0a7157971fdb64bc67083d2701b457bd9603bbb4
Ekaterina Serkova <48721923+KateSerkova@users.noreply.github.com>
2022-05-08 22:45:21 +0300

    Ответ на задание №4. Создан класс Contact Data

--- adressbook-web-tests/src/test/java/ru/se/addressbook/AddingContactTests.java
+++ adressbook-web-tests/src/test/java/ru/se/addressbook/AddingContactTests.java
@@ -16,6 +16,7 @@ public class AddingContactTests {
     wd.get("http://localhost/addressbook/");
     login("admin", "secret");
   }
+  
   private void login(String userName, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -28,29 +29,43 @@ public class AddingContactTests {
 
   @Test
   public void testAddingContact() throws Exception {
-    wd.findElement(By.linkText("add new")).click();
-    wd.get("http://localhost/addressbook/edit.php");
+    goToCreateAccountPage();
+    fillCreateContactForm(new ContactData("Sonya", "Se", "Raduzhna", "Zaa", "Microsoft", "Voronezh, Pobedy bulvar, 90", "892082346666", "emailtest@gmail.com"));
+    submitContactCreation();
+    returnToHomePage();
+  }
+
+  private void fillCreateContactForm(ContactData contactData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Liza");
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
     wd.findElement(By.name("middlename")).click();
     wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys("Se");
+    wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddleName());
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Olegovna");
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
     wd.findElement(By.name("nickname")).click();
     wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys("Zaa");
-    wd.findElement(By.name("title")).click();
-    wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys("TitleisHere");
+    wd.findElement(By.name("nickname")).sendKeys(contactData.getNickName());
     wd.findElement(By.name("company")).click();
     wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys("Microsoft");
-    wd.findElement(By.name("submit")).click();
+    wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
+    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+    wd.findElement(By.name("home")).sendKeys(contactData.getPhoneHome());
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+  }
+
+  private void returnToHomePage() {
     wd.findElement(By.linkText("home page")).click();
-    wd.get("http://localhost/addressbook/index.php");
+  }
+
+  private void submitContactCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void goToCreateAccountPage() {
+    wd.findElement(By.linkText("add new")).click();
   }
 
   @AfterMethod(alwaysRun = true)
---
+++ adressbook-web-tests/src/test/java/ru/se/addressbook/ContactData.java
@@ -0,0 +1,55 @@
+package ru.se.addressbook;
+
+public class ContactData {
+  private final String firstName;
+  private final String middleName;
+  private final String lastName;
+  private final String nickName;
+  private final String company;
+  private final String address;
+  private final String phoneHome;
+  private final String email;
+
+  public ContactData(String firstName, String middleName, String lastName, String nickName, String company, String address, String phoneHome, String email) {
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.nickName = nickName;
+    this.company = company;
+    this.address = address;
+    this.phoneHome = phoneHome;
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getNickName() {
+    return nickName;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getPhoneHome() {
+    return phoneHome;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
