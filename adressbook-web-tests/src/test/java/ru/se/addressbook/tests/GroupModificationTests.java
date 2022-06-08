650c3491a05d6d022327e0dde57699bc6cc8254e
Ekaterina Serkova <48721923+KateSerkova@users.noreply.github.com>
2022-06-08 22:39:06 +0300

    Создан тест редактирования группы

---
+++ adressbook-web-tests/src/test/java/ru/se/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,18 @@
+package ru.se.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.se.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase{
+  @Test
+  public void testGroupModification(){
+    app.getNavigationHapler().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test11", "test22", "test33"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+
+
+  }
+}
